0df2597debd93b38b747a674033caa19020a195d
Diego <diego.lavezzari@gmail.com>
2024-08-25 15:18:06 -0300

    dejar original

--- index.js
+++ index.js
@@ -7,15 +7,8 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-<<<<<<< HEAD
-<<<<<<< HEAD
-  res.end('Hola, MundosE! 25/08 DEV nuevo\n');
-=======
-  res.end('Hola, MundosE! 25/08 DEVdev\n');
->>>>>>> e59eb15f28fc6eb0cd6de4859aabf3097d57286a
-=======
-  res.end('Hola, MundosE! 25/08 main\n');
->>>>>>> parent of e4a90fa (main vsc)
+  res.end('Hola, MundosE! 25/08 dev \n');
+parent of e4a90fa (main vsc)
 }); 
 
 // Inicia el servidor y escucha en el puerto configurado
