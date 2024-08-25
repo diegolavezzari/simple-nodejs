cc0ac6c2eb60f08d74d1de20dfe77afd33654640
Diego <diego.lavezzari@gmail.com>
2024-08-25 15:19:20 -0300

    arreglo de branch dev

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola, MundosE! 25/08 dev \n');
-parent of e4a90fa (main vsc)
+  res.end('Hola, MundosE! 25/08 dev original \n');
 }); 
 
 // Inicia el servidor y escucha en el puerto configurado
